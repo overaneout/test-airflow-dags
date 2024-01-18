be6d3b3ec1b14bc4acbf49d93a27a88b408edf8e
overaneout <overaneout@gmail.com>
2024-01-18 18:21:37 +0800

    modified dag

--- dags/test-dag1.py
+++ dags/test-dag1.py
@@ -1,21 +1,7 @@
-from airflow.decorators import task
-from airflow.operators.python import is_venv_installed
+from airflow.operators.python import is_venv_installed, PythonVirtualenvOperator
 from airflow.models.dag import DAG
 from datetime import datetime, timedelta
 
-@task.virtualenv(task_id="test-virtualenv-python",requirements=["driverlessai==1.10.6.2","h2o-mlops==0.62.1a5"], system_site_packages=False)
-def test_virtualenv_call():
-    """
-    a dummy function to pip install files into python-venv
-       
-    """
-    import h2o_mlops as mlops
-    import driverlessai as dai
-
-    print(mlops.__version__)
-    print(dai.__version__)
-
-    print("done")
 
 
 with DAG(
@@ -33,5 +19,17 @@ with DAG(
     start_date=datetime(2024,1,17),
     catchup=False,
 ) as dag:
-    virtualenv_task = test_virtualenv_call()
+    def test_virtualenv_call():
+        """
+        a dummy function to pip install files into python-venv
+        
+        """
+        import h2o_mlops as mlops
+        import driverlessai as dai
 
+        print(mlops.__version__)
+        print(dai.__version__)
+
+        print("done")
+    
+    virtualenv_task = PythonVirtualenvOperator(task_id="test-virtualenv-python",python_callable=test_virtualenv_call,requirements=["driverlessai==1.10.6.2","h2o-mlops==0.62.1a5"], system_site_packages=False)
