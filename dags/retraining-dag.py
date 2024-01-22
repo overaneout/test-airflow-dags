53527d027a57bf7f3e3e47100371232e6c8a967d
overaneout <overaneout@gmail.com>
2024-01-22 21:16:11 +0800

    get task to retrieve drift metric

--- dags/retraining-dag.py
+++ dags/retraining-dag.py
@@ -7,7 +7,8 @@ from airflow.models import Variable
 
 haic_refresh_token = Variable.get("HAIC_REFRESH_TOKEN")
 haic_domain = Variable.get("HAIC_DOMAIN")
-mlops_task_reqs = ["h2o-mlops==0.62.1a5","h2o_authn==1.1.1"]
+deployment_id = Variable.get("TESTING_DEPLOYMENT_ID")
+mlops_task_reqs = ["h2o-mlops==0.62.1a5","h2o_authn==1.1.1","https://s3.amazonaws.com/artifacts.h2o.ai/releases/ai/h2o/mlops/rel-0.63.0/4/h2o_mlops_client-0.63.0-py2.py3-none-any.whl"]
 # dai_task_reqs = ["driverlessai==1.10.6.2"]
 
 with DAG(
@@ -26,33 +27,41 @@ with DAG(
     catchup=False,
 ) as dag:
 
-    def test_virtualenv_call(haic_refresh_token,haic_domain):
+    def test_virtualenv_call(haic_refresh_token,haic_domain,deployment_id):
         """
         a dummy function to pip install files into python-venv
         
         """
-        import h2o_mlops as mlops
+        import h2o_mlops_client as mlops ## Legacy ui to do monitoring
         import h2o_authn as authn
-        print("testing task")
-        # haic_domain = kwargs.get("HAIC_DOMAIN")
-        # haic_refresh_token = kwargs.get("HAIC_REFRESH_TOKEN")
-        print(haic_refresh_token)
-        print(haic_domain)
+        import pytz
+        # print(haic_refresh_token)
+        # print(haic_domain)
+        # print(deployment_id)
+
         token_provider = authn.TokenProvider(
             refresh_token=haic_refresh_token,
-            client_id="hac-platform-public", 
+            client_id="hac-platform-public",
             token_endpoint_url=f"http://auth.{haic_domain}/auth/realms/hac/protocol/openid-connect/token" 
             )
         mlops_client = mlops.Client(
             gateway_url=f"http://mlops-api.{haic_domain}",
             token_provider=token_provider
         )
-        print("mlops projects")
-        print(mlops_client.projects.list())
+        end_time = datetime.datetime.now(pytz.utc)
+        start_time = end_time - datetime.timedelta(days=30)
+        drift_metrics = mlops_client._backend._model_monitoring._model_monitoring_service.get_model_drift_metrics(deployment_id=deployment_id,
+                                                                                                  start_date_time=start_time,
+                                                                                                  end_date_time=end_time,
+                                                                                                  model_id=deployment_id) 
+        print(drift_metrics)
+
+
     
     virtualenv_task = PythonVirtualenvOperator(task_id="test-mlops-connection",
                                                python_callable=test_virtualenv_call,
                                                requirements=mlops_task_reqs, 
                                                system_site_packages=False,
-                                               op_args=[haic_refresh_token,haic_domain]
+                                               op_args=[haic_refresh_token,haic_domain,deployment_id]
                                                )
+    
